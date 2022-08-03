dc92bc0bfd24583f7289765f17d81cd932b28fa6
252505Sonali@ <praveshkumar1062002@gmail.com>
2022-08-03 20:36:59 +0530

    Material UI Added

---
+++ src/ToDoListAPP/ToDoListspecail.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const  TodoListspecail=()=>{
+    return(
+            <>
+            <div className='mian_div'>
+                <div className='cneter_div'>
+                    <br/>
+                    <h1>ToDo List</h1>
+                    <br/>
+                    <input
+                        type='text'
+                        placeholder='add an  item'
+
+                    />
+
+                   
+                </div>
+             </div>
+
+           </>
+           )
+
+}
+export default TodoListspecail;
--- src/Todolists.jsx
+++ src/Todolists.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './todolistapp.css'
+import AddIcon from '@mui/icons-material/Add';
+import { Add } from "@material-ui/icons";
 const Todolists=(props)=>
 {
     return (<>
             <div className="todo_style">
-             <i className="fa-solid fa-circle-xmark"
+                <AddIcon/>
                 onClick={()=>{
                     return props.onSelect(props.id);
                 }}
